a1337768c9cf7b86e2af28ad43208756f650cf46
KonstantinOlehno <alekhna.ki@gmail.com>
2021-06-10 16:48:01 +0300

    6

--- src/main/java/by/olehno/array/service/ArrayChangeService.java
+++ src/main/java/by/olehno/array/service/ArrayChangeService.java
@@ -6,13 +6,13 @@ import org.apache.logging.log4j.Level;
 
 public class ArrayChangeService {
 
-    public void ChangeElement (CustomArray customArray){
+    public void ChangeElement(CustomArray customArray) {
         int[] array = customArray.getArray();
         for (int i = 0; i < array.length; i++) {
-            if(array[i]%2 == 1){
+            if (array[i] % 2 == 1) {
                 array[i] = -1;
             }
         }
-        Logging.getLogger().log(Level.INFO,"Изменение элементов массива по условию " + customArray.toString());
+        Logging.getLogger().log(Level.INFO, "Изменение элементов массива по условию " + customArray.toString());
     }
 }
